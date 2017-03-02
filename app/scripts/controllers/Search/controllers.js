d0603baf9d51bd4eba1b7b5f3a4b485b955f4030
pcboby <pcboby@126.com>
2017-03-02 17:35:37 +0800

    ree

--- app/scripts/controllers/Search/controllers.js
+++ app/scripts/controllers/Search/controllers.js
@@ -1,7 +1,7 @@
 ;(function(){
 	'use strict';
 	app
-	.controller('Search', function ($scope,$element,$state, $stateParams, NgTableParams, RecordList) {
+	.controller('Search', function ($scope,$element,$state, $stateParams, NgTableParams, SimpleList) {
 
 		// console.log('Search $stateParams',$stateParams)
 		$scope.data = [];
@@ -13,26 +13,40 @@
 		}
 
 		$scope._view=_view;
+        $scope._search=_search;
 
 		$scope.tableParams = new NgTableParams({
             count: 5
         }, {
             counts: [5, 10, 20],
-            // dataset: $scope.data
-            getData: function(params) {
-                return RecordList.get(params.url()).$promise.then(function(res) {
-                    params.total(res.total); // recal. page nav controls
-                    $scope.data = res.rows
-                    return $scope.data;
-                });
-            }
+            dataset: $scope.data
+            // getData: function(params) {
+            //     return RecordList.get(params.url()).$promise.then(function(res) {
+            //         params.total(res.total); // recal. page nav controls
+            //         $scope.data = res.rows
+            //         return $scope.data;
+            //     });
+            // }
         });
+
+        _search();
+
+
+        function _search(){
+            SimpleList.get(function(res){
+                console.log(res)
+                $scope.data = res.rows;
+                $scope.statistics=res.statistics;
+                // $scope.tableParams.settings.dataset=angular.copy($scope.data);
+                // $scope.tableParams.total(res.totals)
+                // $scope.tableParams.reload();
+            })
+        }
 
         function _view(id){
         	console.log('_view');
         	$state.go('web.SearchView',{id:id});
         }
-
 
 	})
 })()
