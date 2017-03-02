4f204f543d39ba2a0bb6ede43eadf8798c3298a0
pcboby <pcboby@126.com>
2017-03-02 22:14:59 +0800

    111
    111

--- app/scripts/controllers/Search/controllers.js
+++ app/scripts/controllers/Search/controllers.js
@@ -1,52 +1,39 @@
-;(function(){
-	'use strict';
-	app
-	.controller('Search', function ($scope,$element,$state, $stateParams, NgTableParams, SimpleList) {
+;
+(function() {
+    'use strict';
+    app
+        .controller('Search', function($scope, $element, $state, $stateParams, NgTableParams, SimpleList) {
 
-		// console.log('Search $stateParams',$stateParams)
-		$scope.data = [];
-		$scope.statistics={
-			record:0,
-			chart:0,
-			track:0,
-			database:0
-		}
+            // console.log('Search $stateParams',$stateParams)
+            $scope.data = [];
+            $scope.statistics = {
+                record: 0,
+                chart: 0,
+                track: 0,
+                database: 0
+            }
 
-		$scope._view=_view;
-        $scope._search=_search;
+            $scope._view = _view;
 
-		$scope.tableParams = new NgTableParams({
-            count: 5
-        }, {
-            counts: [5, 10, 20],
-            dataset: $scope.data
-            // getData: function(params) {
-            //     return RecordList.get(params.url()).$promise.then(function(res) {
-            //         params.total(res.total); // recal. page nav controls
-            //         $scope.data = res.rows
-            //         return $scope.data;
-            //     });
-            // }
-        });
-
-        _search();
+            $scope.tableParams = new NgTableParams({
+                count: 5
+            }, {
+                counts: [5, 10, 20],
+                // dataset: $scope.data
+                getData: function(params) {
+                    return SimpleList.get(params.url()).$promise.then(function(res) {
+                        params.total(res.total); // recal. page nav controls
+                        $scope.data = res.rows
+                        return $scope.data;
+                    });
+                }
+            });
 
 
-        function _search(){
-            SimpleList.get(function(res){
-                console.log(res)
-                $scope.data = res.rows;
-                $scope.statistics=res.statistics;
-                // $scope.tableParams.settings.dataset=angular.copy($scope.data);
-                // $scope.tableParams.total(res.totals)
-                // $scope.tableParams.reload();
-            })
-        }
+            function _view(id) {
+                console.log('_view');
+                $state.go('web.SearchView', { id: id });
+            }
 
-        function _view(id){
-        	console.log('_view');
-        	$state.go('web.SearchView',{id:id});
-        }
-
-	})
+        })
 })()
