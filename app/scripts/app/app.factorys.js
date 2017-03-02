d10aa7c9ad6b45f2ef657542d17f7fc4c334db2e
pcboby <pcboby@126.com>
2017-03-03 02:32:40 +0800

    factory $tableParams & $tableParamsLock

--- app/scripts/app/app.factorys.js
+++ app/scripts/app/app.factorys.js
@@ -10,4 +10,33 @@ angular.module('app.factorys', [])
         $rootScope.settings = settings;
 
         return settings;
+    }])
+    .factory('$tableParams', ['NgTableParams',function (NgTableParams) {
+    	return {
+    		creat:function(scope,options){
+    			return new NgTableParams({
+                    count: 5
+                }, angular.extend({
+                    counts: [5, 10, 20],
+                    dataset: null
+                },options,true))
+    		}
+    	};
+    }])
+    .factory('$tableParamsLock', ['$tableParams', function($tableParams) {
+        return {
+            init: function(scope, lockTableObj) {
+                scope.tableParamsLock = $tableParams.creat(scope)
+
+                scope.$watch(function() {
+                    return scope.tableParamsLock.sorting();
+                }, function(sort) {
+                    lockTableObj.sorting(sort);
+                }, true)
+
+            },
+            settings: function(scope, settings) {
+                scope.tableParamsLock.settings(settings)
+            }
+        };
     }]);
--- app/scripts/controllers/Record/controllers.js
+++ app/scripts/controllers/Record/controllers.js
@@ -1,30 +1,31 @@
-;(function() {
+;
+(function() {
     'use strict';
     app
-        .controller('Record', function($scope,$element,$state, $stateParams, NgTableParams, RecordList) {
+        .controller('Record', function($scope, $element, $state, $stateParams, $tableParams, $tableParamsLock, RecordList) {
 
 
             $scope.forms = {
-                userName:$stateParams.userName||'',
-                department:$stateParams.department||'',
-                dateEntryFrom:$stateParams.dateEntryFrom||'',
-                dateEntryUntil:$stateParams.dateEntryUntil||''
+                userName: $stateParams.userName || '',
+                department: $stateParams.department || '',
+                dateEntryFrom: $stateParams.dateEntryFrom || '',
+                dateEntryUntil: $stateParams.dateEntryUntil || ''
             }
 
             $scope.data = [];
-            $scope.checked=0;//已经选中数量
-            $scope.unchecked=0;//未选中数量
-            $scope.total=0;//当前显示数量
+            $scope.checked = 0; //已经选中数量
+            $scope.unchecked = 0; //未选中数量
+            $scope.total = 0; //当前显示数量
 
-            $scope._search=_search;
-            $scope._refresh=_refresh;
-            $scope._reset=_reset;
-            $scope._view=_view;
-            $scope._add=_add;
-            $scope._del=_del;
-            $scope._edit=_edit;
-            $scope._import=_import;
-            $scope._export=_export;
+            $scope._search = _search;
+            $scope._refresh = _refresh;
+            $scope._reset = _reset;
+            $scope._view = _view;
+            $scope._add = _add;
+            $scope._del = _del;
+            $scope._edit = _edit;
+            $scope._import = _import;
+            $scope._export = _export;
 
             $scope.checkboxes = {
                 checked: false,
@@ -32,6 +33,7 @@
             };
 
             $scope.$watch(function() {
+                console.log($scope.checkboxes)
                 return $scope.checkboxes.checked;
             }, function(value) {
                 angular.forEach($scope.data, function(item) {
@@ -42,98 +44,131 @@
             $scope.$watch(function() {
                 return $scope.checkboxes.items;
             }, function(values) {
-            	console.log('items',$scope.checkboxes.items);
+                console.log('items', $scope.checkboxes.items);
                 $scope.checked = 0;
                 $scope.unchecked = 0;
-                $scope.total = $scope.data.length;
+                // $scope.total = $scope.data.length;
                 angular.forEach($scope.data, function(item) {
                     $scope.checked += ($scope.checkboxes.items[item.id]) || 0;
                     $scope.unchecked += (!$scope.checkboxes.items[item.id]) || 0;
                 });
                 if (($scope.unchecked == 0) || ($scope.checked == 0)) {
-                    $scope.checkboxes.checked = ($scope.checked == $scope.total && $scope.checked!=0);
+                    $scope.checkboxes.checked = ($scope.checked == $scope.total && $scope.checked != 0);
                 }
                 // grayed checkbox
                 angular.element($element[0].getElementsByClassName("select-all")).prop("indeterminate", ($scope.checked != 0 && $scope.unchecked != 0));
             }, true);
 
-            $scope.tableParams = new NgTableParams({
-                count: 5
-            }, {
-                counts: [5, 10, 20],
-                // dataset: $scope.data
+            // $scope.tableParams = new NgTableParams({
+            //     count: 5
+            // }, {
+            //     counts: [5, 10, 20],
+            //     // dataset: $scope.data
+            //     getData: function(params) {
+            //         return RecordList.get(params.url()).$promise.then(function(res) {
+            //             params.total(res.total); // recal. page nav controls
+            //             $scope.data = res.rows
+            //             return $scope.data;
+            //         });
+            //     }
+            // });
+
+            $scope.tableParams = $tableParams.creat($scope, {
                 getData: function(params) {
-                    return RecordList.get(params.url()).$promise.then(function(res) {
-                        params.total(res.total); // recal. page nav controls
+                    // console.log('params.url()',params.url())
+                    return RecordList.get(angular.extend(params.url(), {
+                        filter: $stateParams
+                    })).$promise.then(function(res) {
+
+                        console.log(res);
                         $scope.data = res.rows
-                        return $scope.data;
+                        $scope.total = res.total
+
+                        $scope.statistics = res.statistics;
+                        params.total(res.total);
+
+                        $tableParamsLock.settings($scope, {
+                            dataset: res.rows,
+                            page: params.page(),
+                            counts: params.count()
+                        })
+
+                        return res.rows;
                     });
                 }
-            });
+            })
 
-            RecordList.get(function(res){console.log(res)})
+            $tableParamsLock.init($scope, $scope.tableParams);
 
-            function _search(){
-            	console.log('_search',$scope.forms);
-                $state.go('web.Record',$scope.forms)
+            function _search() {
+                console.log('_search', $scope.forms);
+                $state.go('web.Record', $scope.forms)
 
             }
-            function _refresh(){
-            	console.log('_refresh');
-            	$scope.tableParams.reload();
+
+            function _refresh() {
+                console.log('_refresh');
+                $scope.tableParams.reload();
             }
-            function _reset(){
-            	console.log('_reset');
-            	// $scope.tableParams.sorting({}).filter({}).pages(1);
-                for(var e in $scope.forms){
-                    $scope.forms[e]='';
+
+            function _reset() {
+                console.log('_reset');
+                // $scope.tableParams.sorting({}).filter({}).pages(1);
+                for (var e in $scope.forms) {
+                    $scope.forms[e] = '';
                 }
             }
-            function _view(id){
+
+            function _view(id) {
                 console.log('_view');
-                $state.go('web.RecordView',{id:id});
+                $state.go('web.RecordView', { id: id });
             }
-            function _add(){
+
+            function _add() {
                 console.log('_add');
                 $state.go('web.RecordAdd');
             }
-            function _del(id){
-            	console.log('_del',id||getIdsByChecked());
-            	var ids=id||getIdsByChecked();
-            	bootbox.confirm('您确定要删除［'+ids+'］吗？',function(evt){
-            		if(evt){
-            			RecordList.remove({
-            				ids:ids
-            			},function(res){
-            				if(res.status==1){
-            					bootbox.alert('删除成功！')
-            					_refresh();
-            				}else{
-            					bootbox.alert(res.msg);
-            				}
-            			})
-            		}
-            	});
-            }
-            function _edit(id){
-            	console.log('_edit',id);
-            	$state.go('web.RecordEdit',{id:id});
-            }
-            function _import(){
-            	console.log('_import');
-            }
-            function _export(type){
-            	console.log('_export',type);
+
+            function _del(id) {
+                console.log('_del', id || getIdsByChecked());
+                var ids = id || getIdsByChecked();
+                bootbox.confirm('您确定要删除［' + ids + '］吗？', function(evt) {
+                    if (evt) {
+                        RecordList.remove({
+                            ids: ids
+                        }, function(res) {
+                            if (res.status == 1) {
+                                bootbox.alert('删除成功！')
+                                _refresh();
+                            } else {
+                                bootbox.alert(res.msg);
+                            }
+                        })
+                    }
+                });
             }
 
-            function getIdsByChecked(){
-            	var arr=[];
-            	angular.forEach($scope.checkboxes.items, function(item, key){
-            		if(item){
-            			arr.push(key)
-            		}
-            	});
-            	return arr.join();
+            function _edit(id) {
+                console.log('_edit', id);
+                $state.go('web.RecordEdit', { id: id });
+            }
+
+            function _import() {
+                console.log('_import');
+            }
+
+            function _export(type) {
+                console.log('_export', type);
+            }
+
+            function getIdsByChecked() {
+                var arr = [];
+                angular.forEach($scope.checkboxes.items, function(item, key) {
+                    if (item) {
+                        arr.push(key)
+                    }
+                });
+                return arr.join();
             }
 
         })
